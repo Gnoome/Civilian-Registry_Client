b6785bfa6856d26e7ea4df4585ca4a57e515d6dd
gnoome <genannos@gmail.com>
2026-09-01 13:41:54 +0300

    added SendServerRequest & addCivilian methods

--- src/main/java/gr/gnoome/HTTP_Handler.java
+++ src/main/java/gr/gnoome/HTTP_Handler.java
@@ -1,34 +1,54 @@
 package gr.gnoome;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class HTTP_Handler {
 
-    private static void SendServerRequest(String method, String url, String json){
+    public static void SendServerRequest(String method, String url, String json) throws IOException, InterruptedException {
 
         HttpClient client = HttpClient.newHttpClient();
 
         HttpRequest.Builder request = HttpRequest.newBuilder();
         request.uri(URI.create(url));
-        
-        switch(method){
+        request.header("Content-Type", "application/json");
+
+        switch (method) {
             case "POST":
-                request.POST(HttpRequest.BodyPublisher.of);
-            
+                request.POST(HttpRequest.BodyPublishers.ofString(json));
+                break;
+            case "PUT":
+                request.PUT(HttpRequest.BodyPublishers.ofString(json));
+                break;
+            case "GET":
+                request.GET();
+                break;
+            case "DELETE":
+                request.DELETE();
+                break;
 
         }
+
+        HttpResponse<String> response = client.send(request.build(),HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
+
     }
 
+   
 
-    static ObjectMapper mapper = new ObjectMapper();
-    
-    public static void SendCivilian(Person person) throws Exception{
+    public static void SendCivilian(Person person) throws Exception {
+
         
-        String json = mapper.writeValueAsString(person);
+
+        SendServerRequest(Post, json, json);
+
 
     }
 }
